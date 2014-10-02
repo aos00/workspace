4c7930ec01f6ce12441ad91fce707bc62019b194
aos00 <aoliveirasousa0@gmail.com>
2014-10-01 23:20:20 -0300

    1/10
    Calculo da divisão

--- NDVI/principal/win/ndvi/ConsoleApplication1/ConsoleApplication1/nvdi.cpp
+++ NDVI/principal/win/ndvi/ConsoleApplication1/ConsoleApplication1/nvdi.cpp
@@ -10,6 +10,9 @@ using namespace cv;
 int main(int argc, const char** argv){
 	CvCapture* capture = 0;
 	Mat frame;
+	int linha, coluna;
+
+	
 	
 
 
@@ -17,12 +20,10 @@ int main(int argc, const char** argv){
 	
 
 	frame2.open(CV_CAP_ANY);
-	frame2.set(CV_CAP_PROP_FRAME_WIDTH, 320);
-	frame2.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	frame2.set(CV_CAP_PROP_FRAME_WIDTH, 80);
+	frame2.set(CV_CAP_PROP_FRAME_HEIGHT, 60);
 
 	//capture = cvCaptureFromCAM(CV_CAP_ANY);
-	
-	//capture = cvCaptureFromCAM(1);
 
 	if (!frame2.isOpened()){
 		cout << "Nenhuma camera detectada" << endl;
@@ -30,43 +31,54 @@ int main(int argc, const char** argv){
 	}else{
 		cout << "In capure ..." << endl;
 		
-		//for (;;){
+		for (;;){
 		
 			frame2 >> frame;
-					
-			Mat output(frame.size(), frame.type());
 
-			vector<Mat> rgb;
-			
-			
-			split(frame, rgb);
-
-			
 			//Criar Matrizes de float
-			Mat soma(frame.size(), CV_32F);
-			Mat subtracao(frame.size(), CV_32F);
-			Mat red(frame.size(), CV_32F);
-			Mat blue(frame.size(), CV_32F);
-			Mat nvdi(frame.size(), CV_32F);
+			Mat soma(frame.size(), CV_32FC1);
+			Mat subtracao(frame.size(), CV_32FC1);
+			Mat red(frame.size(), CV_32FC1);
+			Mat blue(frame.size(), CV_32FC1);
+			Mat ndvi(frame.size(), CV_32FC1);
+			Mat ndvi2(frame.size(), CV_32FC1);
+			Mat ndvi3(frame.size(), CV_32FC1);
+					
+			//Mat output(frame.size(), frame.type());
+
+			vector<Mat> rgb;		
+			
+			split(frame, rgb);		
 			
 			//Converter valores RGB para float
 			rgb[2].convertTo(red, CV_32F);
 			rgb[0].convertTo(blue, CV_32F);
 
 			//Adicionar e subtrair os canais
-			add(red, blue, soma);
-			subtract(red, blue, subtracao);
+			add(rgb[2], rgb[0], soma);
+			subtract(rgb[2], rgb[0], subtracao);
 
 			//Dividir os canais
-			divide(subtracao, soma);
+			divide(subtracao, soma, ndvi);
+
+			multiply(ndvi, 127, ndvi2);
+
+			add(ndvi2, 1, ndvi3);
 			
-
 			//imshow("subt", sub);
-			//cout << "sub= " << endl << red/blue << endl << endl;
+			//cout << "ndvi" << endl << ndvi << endl << endl;
 
-			imshow("soma", frame);
+			//imshow("soma", ndvi3);
+			cout << "Blue = " << "soma="<< endl << soma << endl;
+			/*cout << "Red = " <<  (int) rgb[2].at<uchar>(100,100) << "; ";
+			cout << "Blue = " << (int) rgb[0].at<uchar>(100, 100) << "; " << endl;*/
+			//cout << "Soma = " << soma.at<float>(100,100) << "; ";
+			/*cout << "Subtracao = " << (int)subtracao.at<double>(100, 100) << "; " << endl;
+			cout << "Divisao = " << (int)ndvi.at<double>(100, 100) << "; ";
+			cout << "offset = " << (int)ndvi3.at<double>(100, 100) << "; " << endl;*/
 
-			while(1);
+
+			//while(1);
 
 			
 
@@ -74,9 +86,9 @@ int main(int argc, const char** argv){
 
 			
 
-			//if (waitKey(10) >= 0)
-				//break;
-		//}
+			if (waitKey(10) >= 0)
+				break;
+		}
 		waitKey(0);
 
 	}
