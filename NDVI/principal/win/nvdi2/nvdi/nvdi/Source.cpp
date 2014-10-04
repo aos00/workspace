9d31d7d65657f5c2bbf75082f178e23b6425da45
aos00 <aoliveirasousa0@gmail.com>
2014-10-04 19:07:13 -0300

    NVDI terminado - implementando funções de cálculo

--- NDVI/principal/win/nvdi2/nvdi/nvdi/Source.cpp
+++ NDVI/principal/win/nvdi2/nvdi/nvdi/Source.cpp
@@ -1,11 +1,222 @@
-//#include <opencv2/objdetect/objdetect.hpp>
+//putText(result, "Differencing the two images.", cvPoint(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200, 200, 250), 1, CV_AA);
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
 
+#define RUIDO_INFERIOR 5
+
 using namespace std;
 using namespace cv;
+
+
+//----CALCULO DO NDVI COM WEBCAM-----------------------
+
+void blueIteration(Mat& I);
+void redIteration(Mat& I);
+void antiLowerNoise(Mat& I);
+void ndviCalculation(Mat& src, Mat& dst);
+
+Size frameSize;
+
+int main(int argc, const char** argv){
+	short limiteInferior = 3;
+	Mat frame;	
+
+	VideoCapture capture(CV_CAP_ANY);
+
+	CV_Assert(capture.isOpened());
+
+	//if (!capture.isOpened()){
+	//		cout << "Nenhuma camera detectada" << endl;
+
+	//	}else{
+			cout << "In capure ..." << endl;
+			
+			frameSize = frame.size();
+
+			Mat soma(frameSize, CV_32F);
+			Mat subtracao(frameSize, CV_32F);
+			Mat ndvi(frameSize, CV_32F);
+
+			for (;;){
+			
+				capture >> frame;
+				//cout << "Is continuous: " << frame.isContinuous() << endl;
+				//Criar Matrizes de float
+				
+
+				vector<Mat> rgb;		
+				split(frame, rgb);		
+				
+				//Adicionar e subtrair os canais
+				add(rgb[2], rgb[0], soma, noArray(), CV_32F);
+
+				antiLowerNoise(soma); // Evitar ruido de baixa intensidade;
+
+				subtract(rgb[2], rgb[0], subtracao, noArray(), CV_32F);
+
+				//Dividir os canais
+				divide(subtracao, soma, ndvi, 1, CV_32F);
+
+				add(ndvi, 1, ndvi, noArray(), CV_32F);
+				multiply(ndvi, 127, ndvi, 1.0, CV_32F);
+
+				ndvi.convertTo(ndvi, CV_8U);
+
+				Mat ndviImage[3];
+
+				ndviImage[2] = (ndvi - 128) * 2; //Canal do vermelho
+				ndviImage[0] = ((255 - ndvi) - 128) * 2; //Canal do azul
+				
+				redIteration(ndviImage[2]);
+				blueIteration(ndviImage[0]);
+
+				ndviImage[1] = 255 - (ndviImage[2] + ndviImage[0]);
+
+				Mat imagem;
+				merge(ndviImage, 3, imagem);
+
+
+				//cout << "Canais: " << ndvi.channels() << endl;
+				imshow("NDVI", imagem);
+
+				/*Point3_<uchar> *p = imagem.ptr < Point3_<uchar>(300, 200);
+
+				cout << "ndvi1 " <<  << endl;
+				cout << "ndvi2 " << (int)imagem.at<uchar>(50, 50) << endl;
+				cout << "ndvi3 " << (int)imagem.at<uchar>(200, 300) << endl;*/
+
+				//system("pause");
+				
+				if (waitKey(10) >= 0)
+					break;
+			}
+			waitKey(0);
+
+		//}
+		//cvReleaseCapture(&capture);
+		//cvDestroyWindow("NDVI");
+
+
+	return 0;
+}
+
+void ndviCalculation(Mat& src, Mat& dst){
+	
+	CV_Assert(src.channels() == 3); //Garantir que o frame possui 3 canais
+	CV_Assert(dst.channels() == 1); //Garantir que a matriz de saida tem 1 canal (Grayscale)
+	CV_Assert(src.isContinuous()); //Garantir que a matriz é continua
+
+	int nCols = src.cols * 3;
+
+	Mat rgb[3];
+	Mat soma(frameSize, CV_32F);
+	Mat subtracao(frameSize, CV_32F);
+	Mat ndvi(frameSize, CV_32F);
+
+	split(src, rgb);
+
+	//Adicionar e subtrair os canais
+	add(rgb[2], rgb[0], soma, noArray(), CV_32F);
+
+	antiLowerNoise(soma); // Evitar ruido de baixa intensidade;
+
+	subtract(rgb[2], rgb[0], subtracao, noArray(), CV_32F); // fazer subtracao
+
+	//Dividir os canais
+	divide(subtracao, soma, ndvi, 1, CV_32F);
+
+	add(ndvi, 1, ndvi, noArray(), CV_32F);
+	multiply(ndvi, 127, ndvi, 1.0, CV_32F);
+
+	ndvi.convertTo(ndvi, CV_8U);
+
+	
+
+
+
+}
+
+void antiLowerNoise(Mat& I){
+	int channels = I.channels();
+
+	int nRows = I.rows;
+	int nCols = I.cols * channels;
+
+	if (I.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	int i, j;
+	uchar* p;
+	for (i = 0; i < nRows; ++i)
+	{
+		p = I.ptr<uchar>(i);
+		for (j = 0; j < nCols; ++j)
+		{
+			if (p[j] < RUIDO_INFERIOR)
+				p[j] = RUIDO_INFERIOR;
+		}
+	}
+}
+
+void blueIteration(Mat& I){
+	int channels = I.channels();
+
+	int nRows = I.rows;
+	int nCols = I.cols * channels;
+
+	if (I.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	int i, j;
+	uchar* p;
+	for (i = 0; i < nRows; ++i)
+	{
+		p = I.ptr<uchar>(i);
+		for (j = 0; j < nCols; ++j)
+		{
+			if (p[j] >= 128)
+				p[j] = 0;
+		}
+	}
+}
+
+void redIteration(Mat& I)
+{
+	// accept only char type matrices
+	//CV_Assert(I.depth() != sizeof(uchar));
+
+	int channels = I.channels();
+
+	int nRows = I.rows;
+	int nCols = I.cols * channels;
+
+	if (I.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	int i, j;
+	uchar* p;
+	for (i = 0; i < nRows; ++i)
+	{
+		p = I.ptr<uchar>(i);
+		for (j = 0; j < nCols; ++j)
+		{
+			if (p[j] < 128)
+				p[j] = 0;
+		}
+	}
+}
+
 
 
 //-----------INICIANDO CAPTURA CAMERA -------------
@@ -33,12 +244,15 @@ using namespace cv;
 //}
 
 //---------------MANIPULANDO MATRIZES DE CONSTANTES ----------
+
+//Mat& iterate(Mat& I);
+//
 //int main(int argc, const char** argv){
 //
 //	Mat red(10, 10, CV_32F, Scalar(255));
 //	Mat blue(red.size(), CV_32F, Scalar(255));
 //
-//	Mat soma(red.size(), CV_32F);
+//	/*Mat soma(red.size(), CV_32F);
 //	Mat divisao(red.size(), CV_32F);
 //	Mat subtracao(red.size(), CV_32F);
 //
@@ -52,12 +266,47 @@ using namespace cv;
 //	add(red, 1, red, noArray(), CV_32F);
 //	cout << "adicao" << red << endl << endl;
 //	multiply(red, 127, red, 1, CV_32F);
-//	cout << "tela" << red << endl << endl;
-//	imshow("matriz", divisao);
+//	cout << "tela" << red << endl << endl;*/
+//
+//	iterate(red);
+//	cout << "red" << red << endl << endl; 
+//	imshow("matriz", red);
 //
 //	system("pause");
 //
 //	return 0;
+//}
+//
+//Mat& iterate(Mat& I)
+//{
+//	// accept only char type matrices
+//	CV_Assert(I.depth() != sizeof(uchar));
+//
+//	int channels = I.channels();
+//
+//	int nRows = I.rows;
+//	int nCols = I.cols * channels;
+//
+//	if (I.isContinuous())
+//	{
+//		nCols *= nRows;
+//		nRows = 1;
+//	}
+//
+//	int i, j;
+//	float* p;
+//	for (i = 0; i < nRows; ++i)
+//	{
+//		p = I.ptr<float>(i);
+//		for (j = 0; j < nCols; ++j)
+//		{
+//			cout << (float)p[j] << endl;
+//			if (p[j] > 100)
+//				p[j] = 0;
+//			//p[j] > 100 ? 0 : p[j];
+//		}
+//	}
+//	return I;
 //}
 
 	//------------------------------------------------------------------------------
@@ -89,66 +338,10 @@ using namespace cv;
 	*/
 
 
-	//----CALCULO DO NDVI COM WEBCAM-----------------------
-
-	int main(int argc, const char** argv){
-		
-		Mat frame;
 	
-		VideoCapture capture(CV_CAP_ANY);
-	
-		if (!capture.isOpened()){
-				cout << "Nenhuma camera detectada" << endl;
-	
-			}else{
-				cout << "In capure ..." << endl;
-				
-				for (;;){
-				
-					capture >> frame;
-	
-					//Criar Matrizes de float
-					Mat soma(frame.size(), CV_32F);
-					Mat subtracao(frame.size(), CV_32F);
-					Mat ndvi(frame.size(), CV_32F);
-	
-					vector<Mat> rgb;		
-					split(frame, rgb);		
-					
-					//Adicionar e subtrair os canais
-					add(rgb[2], rgb[0], soma, noArray(), CV_32F);
-					subtract(rgb[2], rgb[0], subtracao, noArray(), CV_32F);
-	
-					//Dividir os canais
-					divide(subtracao, soma, ndvi, 1, CV_32F);
-					add(ndvi, 1, ndvi, noArray(), CV_32F);
-					multiply(ndvi, 127, ndvi, 1.0, CV_32F);
-
-					cvtColor(ndvi, ndvi, CV_GRAY2RGB, 3);
-
-					imshow("NDVI", ndvi);
-	
-					cout << "ndvi " << ndvi.at<float>(300,200) << endl;
-					cout << "ndvi " << ndvi.at<float>(50, 50) << endl;
-					cout << "ndvi " << ndvi.at<float>(200, 300) << endl;
-	
-					//system("pause");
-
-					if (waitKey(10) >= 0)
-						break;
-				}
-				waitKey(0);
-	
-			}
-			//cvReleaseCapture(&capture);
-			//cvDestroyWindow("NDVI");
-	
-	
-		return 0;
-	}
 
 
-//-----------------------------------------------------------------------------------
+//------------------------TESTANDO WEBCAM-----------------------------------------------------------
 
 //int main(int argc, const char** argv){
 //	VideoCapture capture;
