2140cc0f9e6e1a4216174d2ab7cea036d0876a05
aos00 <aoliveirasousa0@gmail.com>
2014-11-03 00:45:53 +0000

    first raspi repo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+*	Main Source File.
+*	@Author: ANDERSON OLIVEIRA SOUSA.
+*	@Date: October, 2014.
+*/
+
+#include <stdio.h>
+#include "gopro.h"
+
+int main(){
+
+GoPro camera;
+camera.takePicture();
+
+return 0;
+
+
+}
